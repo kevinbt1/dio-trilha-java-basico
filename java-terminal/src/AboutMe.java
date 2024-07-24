8f0d91d46169724e383f151f4f1363ea1971465e
kevinbt1 <kevin.bragatorres@gmail.com>
2024-07-24 02:03:39 -0300

    update terminal e argumentos

--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -1,9 +1,11 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe {
     public static void main(String[] args) {
-        //criando o objeto scanner
+        try{
+         //criando o objeto scanner
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Digite seu nome");
@@ -18,11 +20,15 @@ public class AboutMe {
         System.out.println("Digite sua altura");
         double altura = scanner.nextDouble();
 
+
         //imprimindo os dados obtidos pelo usuario   
-        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Ola, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
         System.out.println("Tenho " + idade + " anos ");
         System.out.println("Minha altura é " + altura + "cm ");
-
-
+        scanner.close();
+        }
+        catch (InputMismatchException e){
+            System.err.println("Os campos idade e altura precisam ser numéricos");
+        }
     }    
 }
